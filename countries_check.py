14518ff5338726a5d3e08da54aa17243e993e4cc
Giorgio <43949988+GiorgioPizzuto@users.noreply.github.com>
2020-05-17 18:51:23 +0200

    Revert "minor changes"
    This reverts commit 09fcbe5db6eb5c2a3e1ba3622850ff59d7d2552d.

--- countries_check.py
+++ countries_check.py
@@ -5,15 +5,10 @@ import pickle
 from process_functions import adjust_names, aggregate_countries, picklify
 from pathlib import Path
 
-#download, filter and stores the updated list of countries present in the john hopkins files
-
-#download from the JH repo
 url_confirmed = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 df_confirmed = pd.read_csv(url_confirmed)
 
-#filter the countries' names to fit our list of names
 df_confirmed = adjust_names(df_confirmed)
 df_confirmed = aggregate_countries(df_confirmed, graph = 'scatter')
 
-#store in pickle files
 picklify(set(df_confirmed['Country/Region']), 'set_countries_JH')
